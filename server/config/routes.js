355c68200091efa9d2ef8fdd554fcb6e1969755d
Sudhanva Narayana <nsudhanva@gmail.com>
2019-01-31 12:15:39 +0530

    Basic controller path setip

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const { batchesController } = require('../app/controllers/batches-controller');
 const { departmentsController } = require('../app/controllers/departments-controller');
+const { semestersController } = require('../app/controllers/semesters-controller');
+const { sessionsController } = require('../app/controllers/sessions-controller');
+const { subjectsController } = require('../app/controllers/subjects-controller');
+const { timetablesController } = require('../app/controllers/timetables-controller');
 const { usersController } = require('../app/controllers/users-controller');
 
+router.use('/batches', batchesController);
 router.use('/departments', departmentsController);
+router.use('/semesters', semestersController);
+router.use('/sessions', sessionsController);
+router.use('/subjects', subjectsController);
+router.use('/timetablesController', timetablesController);
 router.use('/users', usersController);
 
 module.exports = {
