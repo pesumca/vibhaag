77d99b132a1c14afafc9f081955545dee6fec76c
Sudhanva Narayana <nsudhanva@gmail.com>
2019-02-04 20:29:09 +0530

    Fix mongo db connection bug by config import

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://mongo:27017/server', { useNewUrlParser: true });
+const MONGODB_URI = 'mongodb://localhost:27017/vibhaag';
+
+// mongoose.connect('mongodb://mongo:27017/server', { useNewUrlParser: true });
+
+mongoose.connect(`${MONGODB_URI}`, { 
+    useNewUrlParser: true
+})
+.then(() => {
+    console.log('Connected to DB');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('./config/db');
 
 const bodyParser = require('body-parser');
 const { routes } = require('./config/routes');
