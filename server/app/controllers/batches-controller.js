39b32a6ea29a038d8bc765a5c5484c178c1842ca
Karthik Devaraj <karthikdevaraj1017@gmail.com>
2019-03-19 18:54:04 +0530

    controllers updated

--- server/app/controllers/batches-controller.js
+++ server/app/controllers/batches-controller.js
@@ -4,6 +4,69 @@ const router = express.Router();
 const _ = require('lodash');
 const { Batch } = require('../models/batch');
 
+// get all the batch
+router.get('/', (req, res) => {
+    Batch.find().populate('semester').then(batches => {
+        res.send(batches);
+    })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+// get the specific Batch
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    Batch.findById(id).populate('semester').then(batch => {
+        res.send(batch)
+    })
+});
+
+// delete a Batch
+router.delete('/:id', (req, res) => {
+    let id = req.params.id
+    Batch.findByIdAndRemove(id).then(batch => {
+        if (batch) {
+            res.send({
+                batch,
+                notice: 'successfully deleted batch'
+            });
+        }
+        else {
+            res.send(404).send({
+                notice: 'the batch already removed'
+            });
+        }
+    }).catch(err => {
+        res.send(err);
+    })
+})
+
+// create a batch
+router.post('/', (req, res) => {
+    let body = req.body;
+    let batch = new Batch(body);
+    batch.save().then((batch) => {
+        res.send(batch);
+    })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+// update a specific batch
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    Batch.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(batch => {
+            res.send(batch)
+        }).catch(err => {
+            res.send(err);
+        })
+})
+
+
 module.exports = {
     batchesController: router
 }
--- server/app/controllers/sessions-controller.js
+++ server/app/controllers/sessions-controller.js
@@ -4,6 +4,70 @@ const router = express.Router();
 const _ = require('lodash');
 const { Session } = require('../models/session');
 
+
+// get all the sessions
+router.get('/', (req, res) => {
+    Session.find().populate('batch').populate('user').populate('subject').then(sessions => {
+        res.send(sessions);
+    })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+// get the specific Sessions
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    Session.findById(id).populate('batch').populate('user').populate('subject').then(session => {
+        res.send(session)
+    })
+});
+
+// delete a session
+router.delete('/:id', (req, res) => {
+    let id = req.params.id
+    Session.findByIdAndRemove(id).then(session => {
+        if (session) {
+            res.send({
+                session,
+                notice: 'successfully deleted session'
+            });
+        }
+        else {
+            res.send(404).send({
+                notice: 'the session already removed'
+            });
+        }
+    }).catch(err => {
+        res.send(err);
+    })
+})
+
+// create a session
+router.post('/', (req, res) => {
+    let body = req.body;
+    let session = new Session(body);
+    session.save().then((session) => {
+        res.send(session);
+    })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+// update a specific session
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    Session.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(session => {
+            res.send(session)
+        }).catch(err => {
+            res.send(err);
+        })
+})
+
+
 module.exports = {
     sessionsController: router
 }
