edcde88515d765f2838a3bb13b1228bdd4b693a7
Sudhanva Narayana <nsudhanva@gmail.com>
2019-03-30 00:38:56 +0530

    Login Works

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { Route, withRouter, Switch,Redirect } from 'react-router-dom';
-
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import TopNav from 'Containers/TopNav'
 import Sidebar from 'Containers/Sidebar';
 
-import gogo from './gogo';
-import secondMenu from './second-menu';
+import departments from './departments';
+import DepartmentShow from './departments/show'
+import DepartmentEdit from './departments/edit'
+import sessions from './sessions';
+import SessionShow from './sessions/show'
+import subjects from './subjects';
+import SubjectShow from './subjects/show';
+import users from './users';
+import UserShow from './users/show';
 
 import { connect } from 'react-redux';
+
 
 class MainApp extends Component {
 	constructor(props) {
@@ -15,16 +22,23 @@ class MainApp extends Component {
 	}
 
 	render() {
-		const { match, containerClassnames} = this.props;
+		const { match, containerClassnames } = this.props;
 		return (
 			<div id="app-container" className={containerClassnames}>
 				<TopNav history={this.props.history} />
-				<Sidebar/>
+				<Sidebar />
 				<main>
 					<div className="container-fluid">
 						<Switch>
-							<Route path={`${match.url}/gogo`} component={gogo} />
-							<Route path={`${match.url}/second-menu`} component={secondMenu} />
+							<Route path={`${match.url}/departments`} component={departments} exact={true} />
+							<Route path={`${match.url}/departments/:id`} component={DepartmentShow} exact={true} />
+							<Route path={`${match.url}/departments/:id/edit`} component={DepartmentEdit} exact={true} />
+							<Route path={`${match.url}/sessions`} component={sessions} exact={true} />
+							<Route path={`${match.url}/sessions/:id`} component={SessionShow} exact={true} />
+							<Route path={`${match.url}/subjects`} component={subjects} exact={true} />
+							<Route path={`${match.url}/subjects/:id`} component={SubjectShow} exact={true} />
+							<Route path={`${match.url}/users`} component={users} exact={true} />
+							<Route path={`${match.url}/users/:id`} component={UserShow} exact={true} />
 							<Redirect to="/error" />
 						</Switch>
 					</div>
@@ -34,8 +48,8 @@ class MainApp extends Component {
 	}
 }
 const mapStateToProps = ({ menu }) => {
-	const { containerClassnames} = menu;
+	const { containerClassnames } = menu;
 	return { containerClassnames };
-  }
-  
-  export default withRouter(connect(mapStateToProps, {})(MainApp));
+}
+
+export default withRouter(connect(mapStateToProps, {})(MainApp));
